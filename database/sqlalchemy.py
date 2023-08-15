a1ea54eb83dba7fe291411718dbc97e5bda78007
MakMemEng <yatani.mems4568@gmail.com>
2023-08-15 16:27:32 +0900

    ✨ SQLAlchemy,sqlite,memoryの使用方法まとめ

---
+++ database/sqlalchemy.py
@@ -0,0 +1,44 @@
+import sqlalchemy
+import sqlalchemy.orm
+from sqlalchemy.ext.declarative import DeclarativeBase
+
+engine = sqlalchemy.create_engine("sqllite:///:memory:", echo=True)
+
+Base = DeclarativeBase()
+
+
+class Person(Base):
+    __tablename__ = "persons"
+    id = sqlalchemy.Column(
+        sqlalchemy.Integer, primary_key=True, autoincrement=True
+    )
+    name = sqlalchemy.Column(sqlalchemy.String(14))
+
+
+Base.metadata.create_all(engine)
+
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
+session = Session()
+
+p1 = Person(name="Mike")
+session.add(p1)
+p2 = Person(name="Nancy")
+session.add(p2)
+p3 = Person(name="Jun")
+session.add(p3)
+# person = Person(name="Mike")
+# session.add(person)
+session.commit()
+
+p4 = session.query(Person).filter_by(name="Mike").first()
+p4.name = "Michel"
+session.add(p4)
+session.commit()
+
+p5 = session.query(Person).filter_by(name="Nancy").first()
+session.delete(p5)
+session.commit()
+
+persons = session.query(Person).all()
+for person in persons:
+    print(person.id, person.name)
